8f5b23274d439dd29b0014da8e7a48f4f7f174dc
RobHayward <robohay@gmail.com>
2018-04-15 16:25:05 +0100

    Working on the lab and crime paper I want to cumulate wrs. It is difficult.

--- R/intro.R
+++ R/intro.R
@@ -1,10 +1,22 @@
 # Basic work on the data
-#install.package('foreign')
+#install.packages('foreign')
+#install.packages('plm')
 # There is some discussion of how to use it 
 # https://www.statmethods.net/input/importingdata.html
-requuire('foreign')
+# to download stata files
+require('foreign')
+# to use panel methods
+require('plm')
 da1 <- read.dta('./Data/crime_immig.dta')
-head(da1)
+head(da1, n = 10)
+da1$cwrs <- 0
+ifelse(da1$year == 1994, da1$cwrs <- da1$wrs, da1$cwrs <- da1$wrs + lag(da1$cwrs, -1))
 da2 <- read.dta('./Data/police_arrests.dta')
 head(da2)
 # The description of the data is in ./Data/README.rtf
+da1p <- pdata.frame(da1, index = c('la_code', 'year'), drop.index = TRUE, row.names = TRUE)
+head(da1p)
+summary(da1p)
+# To create the matrix that will hold the wrs data prior to cumulating. 
+tempmatrix <- t(as.matrix(da1p$wrs))
+head(tempmatrix)
